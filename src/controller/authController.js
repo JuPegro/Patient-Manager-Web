0e82f60e13c01b6a1874ef8cc98d068a6fad5af2
JuPegro <jupegro.contact@gmail.com>
2024-04-27 23:59:34 -0400

    authController

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,63 +1,64 @@
+const _ = require("lodash");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// ⚡ MIDDLEWARE FOR LOGIN USER ⚡
+// MIDDLEWARE FOR LOGIN USER
 const secretKey = process.env.JWT_SECRET_KEY;
 const expireKey = process.env.EXPIRE_IN_TOKEN;
 const saltRounds = process.env.SALT;
 
-// ⚡ MIDDLEWARE FOR LOGIN USER ⚡
+// MIDDLEWARE FOR LOGIN USER
 exports.SignIn = async (req, res, next) => {
   const { username, password } = req.body;
 
-  // ⚡ EMPTY FIELDS ⚡
+  // EMPTY FIELDS
   if (!username || !password) {
     return res.status(422).json({ message: "Empty Fields" });
   }
 
-  // ⚡ SEARCH USER FOR USERNAME ⚡
+  // SEARCH USER FOR USERNAME
   const user = await prisma.user.findUnique({ where: { username } });
 
-  // ⚡ FOUND THE USER ⚡
+  // FOUND THE USER
   if (!user) {
     return res.status(404).json({ message: "User Not Found!" });
   }
 
-  // ⚡ ENCRYPT THE [PASSWORD] PASSED BY THE USER ⚡
+  // ENCRYPT THE [PASSWORD] PASSED BY THE USER
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  // ⚡ INCORRECT PASSWORD ⚡
+  // INCORRECT PASSWORD
   if (!isPasswordValid) {
     return res.status(401).json({ message: "Invalid Credentials" });
   }
 
-  // ⚡ SET JWT FOR USER ⚡
+  // SET JWT FOR USER
   const token = jwt.sign({ Id: user.id }, secretKey, { expiresIn: expireKey });
   res.status(200).json({ auth: true, token, user });
 };
 
 // --------------------------------------------------!
 
-// ⚡ MIDDLEWARE FOR CREATE A NEW USER ⚡
+// MIDDLEWARE FOR CREATE A NEW USER
 exports.SignUp = async (req, res, next) => {
   const { name, lastname, username, email, password, picture } = req.body;
 
-  // ⚡ EMPTY FIELDS ⚡
+  // EMPTY FIELDS
   if (!name || !lastname || !username || !email || !password || !picture) {
     return res.status(422).json({ message: "Empty Fields" });
   }
 
-  // ⚡ FOUND AN EXISTING USER WITH THE SAME USERNAME OR EMAIL
+  // FOUND AN EXISTING USER WITH THE SAME USERNAME OR EMAIL
   const existingUser = await prisma.user.findFirst({
     where: {
       OR: [{ username: username }, { email: email }],
     },
   });
 
-  // ⚡ IF EXISTS RETURN ERROR ⚡
+  // IF EXISTS RETURN ERROR
   if (existingUser) {
     if (existingUser.username === username) {
       return res.status(401).json({ message: "Username already in use" });
@@ -67,7 +68,7 @@ exports.SignUp = async (req, res, next) => {
     }
   }
 
-  // ⚡ ENCRYPT PASSWORD ⚡
+  // ENCRYPT PASSWORD
   const hashedPassword = await bcrypt.hash(password, parseInt(saltRounds));
 
   const user = await prisma.user.create({
@@ -86,22 +87,33 @@ exports.SignUp = async (req, res, next) => {
 
 // --------------------------------------------------!
 
-// ⚡ MIDDLEWARE PROTECTED ROUTES AUTHORIZED ⚡
+// MIDDLEWARE PROTECTED ROUTES AUTHORIZED
 exports.verifyToken = async (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(403).json({ message: 'Token not provided' });
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(403).json({ message: "Token not provided" });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid Token" });
     }
 
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Invalid Token' });
-        }
-        req.id = decoded.id;
-        next();
-    });
-}
+    // SAVE TOKEN WITH USER DATA IN REQUEST [id]
+    req.id = decoded.Id;
+    next();
+  });
+};
 
 exports.Profile = async (req, res, next) => {
-    // WORKING THIS MIDDLEWARE.....
-}
+  //  GET THE ID FROM THE REQ.ID [verifyToken]
+  const user = await prisma.user.findUnique({ where: { id: req.id } });
+
+  if (!user) {
+    return res.status(404).json({ message: "User Not Found" });
+  }
+
+  // [_.omit] RETURN ONLY NECESSARY INFOMATION
+  const userWithoutPassword = _.omit(user, "password");
+  return res.json({ user: userWithoutPassword });
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 
-// ⚡ IMPORT THE CONTROLLER [AUTH] ⚡
+// IMPORT THE CONTROLLER [AUTH]
 const authController = require("../controller/authController");
 
-// ⚡ ACCESS USER [AUTH] ⚡
+// ACCESS USER [AUTH]
 router.post("/signin", authController.SignIn);
 
-// ⚡ REGISTER NEW USER [AUTH] ⚡
+// REGISTER NEW USER [AUTH]
 router.post("/signup", authController.SignUp);
 
-// ⚡ VIEW PROFILE USER [AUTH] ⚡
+// VIEW PROFILE USER [AUTH]
 router.get("/profile", authController.verifyToken, authController.Profile);
 
 module.exports = router;
