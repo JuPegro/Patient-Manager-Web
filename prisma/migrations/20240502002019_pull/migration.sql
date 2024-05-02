5f0c31212b6582944c443d3447d701c72d0723ad
Julio Peguero <jupegro.contact@gmail.com>
2024-05-01 20:53:40 -0400

    Table Doctor

---
+++ prisma/migrations/20240502002019_pull/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `id` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastname` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `picture` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `role` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `status` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - Added the required column `updated_at` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `user_id` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "createdAt",
+DROP COLUMN "id",
+DROP COLUMN "lastname",
+DROP COLUMN "name",
+DROP COLUMN "picture",
+DROP COLUMN "role",
+DROP COLUMN "status",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "user_id" TEXT NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("user_id");
+
+-- DropEnum
+DROP TYPE "Role";
+
+-- DropEnum
+DROP TYPE "Status";
---
+++ src/controller/doctorController.js
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+//exports.getAllDoctor = async (req, res, next) => {};
