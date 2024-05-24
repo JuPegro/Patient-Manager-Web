adc7c5fcca6cd7b89a820854e606dad445994cfe
Julio Peguero <jupegro.contact@gmail.com>
2024-05-23 21:41:30 -0400

    Get Appointments

--- src/controller/appointmentController.js
+++ src/controller/appointmentController.js
@@ -1,4 +1,23 @@
 const { PrismaClient } = require("@prisma/client");
 
-
 const prisma = new PrismaClient();
+
+//MIDDLEWARE GET ALL APPOINTMENTS
+exports.getAllAppointments = async (req, res, next) => {
+  try {
+    //FETCHING DATA TABLE [APPOINTMENT]
+    const appointment = await prisma.appointment.findMany();
+
+    //IF DON'T HAVE APPOINTMENT
+    if (appointment.length === 0) {
+      return res.status(404).json({ message: "Appointments not found!" });
+    }
+
+    return res
+      .status(200)
+      .json({ totalAppointment: appointment.length, appointment });
+  } catch (err) {
+    console.log({ error: "Error fetching Appointments", err });
+    return res.status(500).json({ error: "Internal error server", err });
+  }
+};
--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,6 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
+// IMPORT CONTROLLER [AUTH]
+const authController = require('../controller/authController');
+
+// IMPORT CONTROLLER [PATIENT]
+const appointmentController = require('../controller/appointmentController');
+
+router.get('/appointments', authController.verifyToken, appointmentController.getAllAppointments); //GET ALL APPOINTMENTS
 
 
 module.exports = router;
